b70ba106b61262626008cc454754dac8ad4f9615
asean092000 <anthonynguyen092000@gmail.com>
2022-12-03 16:28:26 +0800

    Chore: add eslint, prettier, pre-commit config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,23 @@
 module.exports = {
+  extends: [
+    "airbnb-typescript",
+    "airbnb/hooks",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:jest/recommended",
+    "prettier",
+    "prettier/@typescript-eslint",
+    "plugin:prettier/recommended"
+  ],
+  plugins: ["prettier", "@typescript-eslint", "jest"],
   env: {
     browser: true,
-    es2021: true,
+    es6: true,
+    jest: true
   },
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
-  overrides: [],
-  parser: "@typescript-eslint/parser",
   parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
+    project: './tsconfig.json'
   },
-  plugins: ["@typescript-eslint"],
-  rules: {},
-};
+  rules: {
+    'prettier/prettier': 'error'
+  }
+}
