fa8b52f83d6562000ef4b5b5c3933ab140636713
asean092000 <anthonynguyen092000@gmail.com>
2022-12-26 19:03:05 +0800

    Update change core to system name

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from "@nestjs/common";
 import { MainModule } from "./components/main.module";
-import { ConfigSystemModule } from "./core/config.system/config.module";
-import { LoggerModule } from "./core/middleware/logger.module";
+import { ConfigSystemModule } from "./system/config.system/config.module";
+import { LoggerModule } from "./system/middleware/logger.module";
 import { ConfigModule } from "@nestjs/config";
 import { typeOrmAsyncConfig } from './config.database/typeorm.config';
 import { TypeOrmModule } from "@nestjs/typeorm";
