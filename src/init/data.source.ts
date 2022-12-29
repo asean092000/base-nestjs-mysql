3fa225918557c53d3fd45a8338ff2991f652f960
asean092000 <anthonynguyen092000@gmail.com>
2022-12-29 18:59:50 +0800

    Improve refesh_token

--- src/init/data.source.ts
+++ src/init/data.source.ts
@@ -2,14 +2,13 @@ import "reflect-metadata";
 import { DataSource } from "typeorm";
 import { User } from "src/components/user/user.entity";
 import { Permission } from "src/components/permission/permission.entity";
-import dataConfig from './data.config'
 export const AppDataSource = new DataSource({
   type: "mysql",
-  host: dataConfig().host,
-  port: dataConfig().port,
-  username: dataConfig().username,
-  password: dataConfig().password,
-  database: dataConfig().database,
+  host: 'db',
+  port: 3306,
+  username: 'root',
+  password: 'mauFJcuf5dhRMQrjj',
+  database: 'nestjs_core',
   synchronize: true,
   logging: true,
   entities: [User, Permission],
